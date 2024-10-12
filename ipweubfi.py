ef7da8eef6bfd882a20f376f7dcb14a5c63711e7
7o1234 <cbabitzke1@gmail.com>
2024-10-12 00:57:16 -0500

    added stuff yk ?!

--- ipweubfi.py
+++ ipweubfi.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 
-df = pd.read_csv("strong.csv")
+#overtime you would expect the trend line to be around the targeted rep range
+#how do you figure out if two lines are the same?
+
+df = pd.read_csv("workout-thing/strong.csv")
 exercises = df["Exercise Name"].unique()
 
 count = 0
 for i in exercises:
     print(f"{count+1}: {i}")
     count += 1
+
 
 
 x = int(input("Choose exercise: "))
@@ -17,5 +22,16 @@ while x < 1 or x > len(exercises):
 
 data = df.loc[df["Exercise Name"] == exercises[x-1]]
 data = data.sort_values(by=["Set Order"])
-data.plot.scatter(x='Weight', y='Reps', title = exercises[x-1])
+
+slope, intercept, r, p, std_err = stats.linregress(data["Weight"], data["Reps"])
+
+def myfunc(x):
+  return slope * x + intercept
+
+mymodel = list(map(myfunc, data["Weight"]))
+
+
+plt.scatter(data["Weight"], data["Reps"])
+plt.plot(data["Weight"], mymodel)
+#data.plot.scatter(x='Weight', y='Reps', title = exercises[x-1])
 plt.show()
